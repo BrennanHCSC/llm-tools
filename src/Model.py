4a1cc5fec8e9837a86cd91f486a913d88781ca63
Patrick Gavigan <Patrick.Gavigan@hc-sc.gc.ca>
2023-07-11 17:18:43 -0400

    debugging

--- src/Model.py
+++ src/Model.py
@@ -1,49 +1,51 @@
 import torch
+import gc
 from peft import PeftModel
 from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer
 import transformers
 import logging
+import os
+import huggingface_hub
 
 from prompter import Prompter
 
-import huggingface_hub
-import os
-
 class Model:
-
-    def __init__(self, forceCPU = True, promptTemplate = ""):
-        if forceCPU:
-            self.device = "cpu"
-        elif torch.cuda.is_available():
-            self.device = "cuda"
-        else:
-            self.device = "cpu"
-        try:
-            if torch.backends.mps.is_available():
-                self.device = "mps"
-        except:  # noqa: E722
-            pass
-
-        self.loadModel()
-        self.promptTemplate = promptTemplate
-        self.prompter = Prompter(self.promptTemplate)
+    def __init__(self, 
+                 base_model = 'decapoda-research/llama-7b-hf', 
+                 lora_weights = 'tloen/alpaca-lora-7b', 
+                 promt_template = ""):
+        self.load_model(base_model, lora_weights)
+        self.promt_template = promt_template
+        self.prompter = Prompter(promt_template)
 
     '''
     Load the model based on https://github.com/tloen/alpaca-lora/blob/main/generate.py#L40
     '''
-    def loadModel(self, 
-                  baseModel = 'decapoda-research/llama-7b-hf', 
-                  loraWeights = 'tloen/alpaca-lora-7b'):
-        self.baseModel = baseModel
-        self.loraWeights = loraWeights
-        logging.info('Base model name: ' + str(self.baseModel))
-        logging.info('Lora weights name: ' + str(self.loraWeights))
+    def load_model(self, base_model, lora_weights):
+        self.base_model = base_model
+        self.lora_weights = lora_weights
+        logging.info('Base model name: ' + str(self.base_model))
+        logging.info('Lora weights name: ' + str(self.lora_weights))
         logging.info('Loading tokenizer')
-        self.tokenizer = LlamaTokenizer.from_pretrained(self.baseModel)
+        self.tokenizer = LlamaTokenizer.from_pretrained(self.base_model)
         logging.info('Loading tokenizer -- Complete')
         logging.info('Loading model')
-        self.loadModelForDevice()
+        
+        try:
+            self.loadModelForDevice("auto")
+        except Exception as e:
+            logging.warn('Loading model, device = ' + str(self.device) + '-- Failed')
+            logging.warn(str(e))
+            try:
+                self.loadModelForDevice("cuda")
+            except Exception as e:
+                logging.warn('Loading model, device = ' + str(self.device) + '-- Failed')
+                logging.warn(str(e))
+                self.loadModelForDevice({'': "cpu"})
+        
+        self.logMemoryUse()
         logging.info('Loading -- Complete')
+
 
     def evaluate(
         self,
@@ -54,11 +56,16 @@ class Model:
         top_k=40,
         num_beams=4,
         max_new_tokens=128,
+        batch_size = 1,
         **kwargs,
     ):
         prompt = self.prompter.generate_prompt(instruction, input)
         inputs = self.tokenizer(prompt, return_tensors="pt")
+        self.model.to('cuda')
         input_ids = inputs["input_ids"].to(self.device)
+        gc.collect()
+        torch.cuda.empty_cache()
+
         generation_config = GenerationConfig(
             temperature=temperature,
             top_p=top_p,
@@ -70,7 +77,7 @@ class Model:
         # Without streaming
         with torch.no_grad():
             generation_output = self.model.generate(
-                input_ids=input_ids,
+                input_ids=input_ids.to('cuda'),
                 generation_config=generation_config,
                 return_dict_in_generate=True,
                 output_scores=True,
@@ -80,53 +87,6 @@ class Model:
         output = self.tokenizer.decode(s)
         yield self.prompter.get_response(output)
        
-    def loadModelForDevice(self, load_8bit = False):
-        logging.info('Loading ' + str(self.device) + ' model')
-        if self.device == "cuda":
-            logging.info('Loading base model')
-            self.model = LlamaForCausalLM.from_pretrained(
-                self.baseModel,
-                load_in_8bit = load_8bit,
-                torch_dtype = torch.float16,
-                device_map="auto",
-            )
-            logging.info('Loading base model -- Complete')
-            logging.info('Loading lora weights')
-            self.model = PeftModel.from_pretrained(
-                self.model,
-                self.loraWeights,
-                torch_dtype = torch.float16,
-            )
-            logging.info('Loading lora weights -- Complete')
-        elif self.device == "mps":
-            logging.info('Loading base model')
-            self.model = LlamaForCausalLM.from_pretrained(
-                self.baseModel,
-                device_map = {"": self.device},
-                torch_dtype = torch.float16,
-            )
-            logging.info('Loading base model -- Complete')
-            logging.info('Loading lora weights')
-            self.model = PeftModel.from_pretrained(
-                self.model,
-                self.loraWeights,
-                device_map = {"": self.device},
-                torch_dtype = torch.float16,
-            )
-            logging.info('Loading lora weights -- Complete')
-        else:
-            logging.info('Loading base model')
-            self.model = LlamaForCausalLM.from_pretrained(
-                self.baseModel, device_map = {"": self.device}, low_cpu_mem_usage = True
-            )
-            logging.info('Loading base model -- Complete')
-            logging.info('Loading lora weights')
-            self.model = PeftModel.from_pretrained(
-                self.model,
-                self.loraWeights,
-                device_map = {"": self.device},
-            )
-            logging.info('Loading lora weights -- Complete')
 
     def getPromptReply(self, prompt):
         reply = self.evaluate(prompt)
@@ -134,3 +94,34 @@ class Model:
         for i in reply:
             out += i
         return out
+
+
+    def logMemoryUse(self):
+        (free_gpu_memory, total_gpu_memory) = torch.cuda.mem_get_info()
+        logging.info('Available CUDA memory: ' + str(free_gpu_memory/ 1024**2) + " MB")
+        logging.info('Total CUDA memory: ' + str(total_gpu_memory/ 1024**2) + " MB")
+        logging.info('total CUDA memory allocated is: {:6.0f}'.format(torch.cuda.memory_allocated() / 1024**2), 'MB') # Returns the current GPU memory occupied by tensors in bytes for a given device.
+        logging.info('maximum CUDA memory allocated is: {:6.0f}'.format(torch.cuda.max_memory_allocated() / 1024**2), 'MB') # Returns the maximum GPU memory occupied by tensors in bytes for a given device.
+        logging.info('total CUDA memory reserved is: {:6.0f}' .format(torch.cuda.memory_reserved()  / 1024**2), 'MB')  # Returns the current GPU memory managed by the caching allocator in bytes for a given device.
+        logging.info('maximum CUDA memory reserved is: {:6.0f}' .format(torch.cuda.max_memory_reserved()  / 1024**2), 'MB') # Returns the maximum GPU memory managed by the caching allocator in bytes for a given device.
+
+
+    def loadModelForDevice(self, device):
+        self.device = device
+        logging.info('Loading base model, device = ' + str(self.device))
+        self.model = LlamaForCausalLM.from_pretrained(
+            self.base_model,
+            load_in_8bit = False,
+            llm_int8_enable_fp32_cpu_offload = True,
+            torch_dtype = torch.float16,
+            device_map = "auto" if(self.device=="cuda") else self.device
+        )
+        logging.info('Loading base model, device = ' + str(self.device) + " -- Complete")
+        logging.info('Loading lora weights, device = ' + str(self.device))
+        self.model = PeftModel.from_pretrained(
+            self.model,
+            self.lora_weights,
+            torch_dtype = torch.float16,
+            #device_map = self.device
+        )
+        logging.info('Loading lora weights, device = ' + str(self.device) + " -- Complete")
