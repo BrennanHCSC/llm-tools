00c3050a83c5cbc4b2904bba2e4993fdb9b3c3bf
Patrick Gavigan <Patrick.Gavigan@hc-sc.gc.ca>
2023-07-12 14:32:44 -0400

    fixed the test case

--- tests/llm_test.py
+++ tests/llm_test.py
@@ -1,10 +1,11 @@
+
 import pytest
 import sys, os
 sys.path.append(os.path.join(sys.path[0],'src'))
 from Model import Model
  
 def test_llmReplyFound():
-    model = Model()
+    model = Model(force_cpu = True)
     prompt = "Name the provinces of Canada"
     reply = model.getPromptReply(prompt)
     assert "Ontario" in reply
